02e6f89cf659410c0d9648b10770cb204a0a76e6
Govind Mishra <krishna.amo@gmail.com>
2020-03-19 10:18:23 +0900

    default page setup

--- src/components/Default.js
+++ src/components/Default.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react'
 
 class Default extends Component {
     render() {
+        console.log(this.props);
         return (
+            
             <div className="container">
                  <div className="row">
                      <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
                          <h1 className="display-3">404</h1>
                          <h1>Error</h1>
                          <h2>Page Not found</h2>
+                         <h3>Tech Requested Url 
+                             <span className="text-danger mx-auto">{this.props.location.pathname}</span>{" "}was not found
+                         </h3>
                      </div>
                  </div>
             </div>
